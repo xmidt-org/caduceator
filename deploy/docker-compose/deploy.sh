f199b99f983bb95432c4da9c7082d446b5113a73
Jasmine Mai <Jasmine.W.Mai.20@dartmouth.edu>
2020-10-05 10:24:20 -0700

    add docker README, update gitignore

--- deploy/docker-compose/deploy.sh
+++ deploy/docker-compose/deploy.sh
@@ -3,16 +3,10 @@
 DIR=$( cd $(dirname $0) ; pwd -P )
 ROOT_DIR=$DIR/../../
 
-pushd $ROOT_DIR
-echo "Building Simulator..."
-docker build -t simulator:local $ROOT_DIR/simulator
-popd
-
 echo "Running services..."
 CADUCEUS_VERSION=${CADUCEUS_VERSION:-local} \
 ARGUS_VERSION=${ARGUS_VERSION:-0.3.6} \
 CADUCEATOR_VERSION=${CADUCEATOR_VERSION:-local} \
-SIMULATOR_VERSION=${SIMULATOR_VERSION:-local} \
 docker-compose -f $ROOT_DIR/deploy/docker-compose/docker-compose.yml up -d $@
 
 sleep 10
@@ -22,10 +16,3 @@ bash config_dynamodb.sh
 
 # TR1D1UM_VERSION=${TR1D1UM_VERSION:-0.1.5} \
 # CADUCEUS_VERSION=${CADUCEUS_VERSION:-0.2.1} \
-
-# echo "Building goaws..."
-# git clone git@github.com:kcajmagic/goaws.git /tmp/goaws
-# pushd /tmp/goaws
-# git checkout adding_http_support
-# docker build -t goaws:local .
-# popd
